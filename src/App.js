16770fb310ef0ff95355ac4045a2098e8d169eea
AnotherDoggedContender <honghawk36@gmail.com>
2024-09-09 10:30:56 +0900

    changed untracked list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import axios from "axios";
 import "./App.css";
 import { useEffect } from "react";
-function App() {}
+function App() {
+    <h1></h1>;
+}
 
 export default App;
