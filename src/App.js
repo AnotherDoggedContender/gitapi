8230a6f5bc3823cc3ddfe9f1fdcf77ee1f501ca4
AnotherDoggedContender <honghawk36@gmail.com>
2024-09-09 11:44:08 +0900

    fix: merged conflict with dev
    merged a confilict with dev branch

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import { useEffect } from "react";
 import { router } from "./routes/router";
 import { RouterProvider } from "react-router-dom";
 function App() {
-    return (<RouterProvider router={router} />;
-    <h1>dev</h1>);
+    return (
+    <RouterProvider router={router} />;
+    <h1>dev</h1>
+    );
 }
 
 export default App;
