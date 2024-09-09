e6b36095606ccb4731b00a178b549f6bd384cf3f
AnotherDoggedContender <honghawk36@gmail.com>
2024-09-09 10:27:21 +0900

    reset token

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import logo from "./logo.svg";
 import axios from "axios";
 import "./App.css";
 import { useEffect } from "react";
-function App() {
-    useEffect(() => {
-        const fetchIssues = async () => {
-            const data = await axios.get(
-                "https://api.github.com/repos/angular/angular-cli", {
-                    headers: {
-                        auth: 
-                    }
-                }
-            );
-            const description = await axios.get("https://api.github.com");
-            if (data && description) {
-                console.log(data);
-                console.log(description);
-            }
-        };
-        fetchIssues();
-    }, []);
-
-    return <div className="App"></div>;
-}
+function App() {}
 
 export default App;
