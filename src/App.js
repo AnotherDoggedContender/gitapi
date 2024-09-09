39df8327cf6d8955b39c122bbc09a9029ad67a85
AnotherDoggedContender <honghawk36@gmail.com>
2024-09-09 12:08:43 +0900

    feat: setted layout
    finished initial setting for layout

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import "./App.css";
 import { useEffect } from "react";
 import { router } from "./routes/router";
 import { RouterProvider } from "react-router-dom";
+import Layout from "./public/layout/layout";
 function App() {
     return (
-        <>
+        <Layout>
             <RouterProvider router={router} />
             <h1>dev</h1>
-        </>
+        </Layout>
     );
 }
 
